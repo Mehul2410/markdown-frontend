d94b977be75e43c6d4f585c1f2083abb8137aeee
Mehul2410 <mehulgawhale18hc@student.mes.ac.in>
2021-07-07 00:56:46 +0530

    push

--- config/index.ts
+++ config/index.ts
@@ -1,1 +1,2 @@
-export const API_URL = process.env.NEXTJS_PUBLIC_URL || "http://localhost:1337";
+export const API_URL =
+  process.env.NEXTJS_PUBLIC_URL || "https://markdown-backend.herokuapp.com";
